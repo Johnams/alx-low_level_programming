5f782c28a31e4b0c9f9bb5e7b8475d382d59d39e
Johnams <namsjohnwande@gmail.com>
2022-09-08 23:08:17 +0000

    tasks6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#!/bin/bash
  #include <stdio.h>
+/**
+ * main- Entry point for the program
+ *
+ * Return: By default returns zero if no errors otherwise returns non-zero value.
+ */
 	int main(void)
 	    {
-		puts("Programming is like building a multilingual puzzle \n");
-		  return 0;
+		puts("\"Programming is like building a multilingual puzzle");
+		  return (0);
 	    }
