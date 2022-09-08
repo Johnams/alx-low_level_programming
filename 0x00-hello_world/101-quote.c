c88f67920f9c6aea2e88f1fe259e83b04ef09404
Johnams <namsjohnwande@gmail.com>
2022-09-08 23:31:50 +0000

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main- program entry
+ *
+ * Return: 1on success
+ */
+int main(void)
+{
+	write(2, "andthat piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main- Entry point for the program
  *
- * Return: By default returns zero if no errors otherwise returns non-zero value.
+ * Return: default returns zero if no errors otherwise returns non-zero value.
  */
 int main(void)
 {
