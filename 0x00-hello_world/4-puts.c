e001e09343a294a53a19c6ac2ed0fecc164ed814
Johnams <namsjohnwande@gmail.com>
2022-09-08 23:16:13 +0000

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *
  * Return: By default returns zero if no errors otherwise returns non-zero value.
  */
-	int main(void)
-	    {
-		puts("\"Programming is like building a multilingual puzzle");
-		  return (0);
-	    }
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
