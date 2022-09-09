ab43fbd811e9a8a95d1950dde7d792e0f6abe1ab
Johnams <namsjohnwande@gmail.com>
2022-09-09 11:14:45 +0000

    tasko

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main- program entry point
  *
- * Return: 1 on success
+ * Return: 1 if successful
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\n", "- Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+}
