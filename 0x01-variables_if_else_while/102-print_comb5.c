18e1afbc336ee71d868a0771c504bd73bd990169
Johnams <namsjohnwande@gmail.com>
2022-09-12 22:42:04 +0000

    task13

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,16 +12,16 @@ int main(void)
 
 	for (num1 = 0; num1 < 100; num1++)
 	{
-		for (num2 = num1 0; num2 < 100; num2++)
+		for (num2 = 0; num2 < 100; num2++)
 		{
 			if (num1 < num2)
 			{
-			putchar((num1 / 10) + '48');
-			putchar((num1 % 10) + '48');
+			putchar((num1 / 10) + 48);
+			putchar((num1 % 10) + 48);
 			putchar(' ');
 
-			putchar((num2 / 10) + '48');
-			putchar((num2 % 10) + '48');
+			putchar((num2 / 10) + 48);
+			putchar((num2 % 10) + 48);
 
 			if (num1 != 98 || num2 != 99)
 				continue;
