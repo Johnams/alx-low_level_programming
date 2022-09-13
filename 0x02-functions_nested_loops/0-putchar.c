6ad203407c0e52b501f1c90bcc7068c2038c5ddd
Johnams <namsjohnwande@gmail.com>
2022-09-13 13:17:25 +0000

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * description- Prints _putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+}
