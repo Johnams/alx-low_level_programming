d57b533881e2c265b794d72ad04377b7953d79cf
Johnams <namsjohnwande@gmail.com>
2022-09-19 11:38:31 +0000

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,13 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int j = 0, i;
 
-	while (s[i++]
+	while (s[i++])
 
-		len++;
+		j++;
 
-	for (i = len - 1; i >= 0; i--)
+	for (i = j - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
