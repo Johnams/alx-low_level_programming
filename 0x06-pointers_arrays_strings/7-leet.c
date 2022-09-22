ccef91c67555b916b62f45a6d2f4e9b435ecf032
Johnams <namsjohnwande@gmail.com>
2022-09-22 12:06:40 +0000

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,17 +14,17 @@ char *leet(char *s)
 	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
 	char trw[5] = {'4', '3', '0', '7', '1'};
 
-	while (x[a])
+	while (s[a])
 	{
 		b = 0;
 
 		while (b < l)
 		{
-			if (x[a] == tr[b] || x[a] - 32 == tr[b]
-				x[a] = trw[b];
+			if (s[a] == tr[b] || s[a] - 32 == tr[b]
+				s[a] = trw[b];
 			b++;
 		}
 		a++;
 	}
-	return (x);
+	return (s);
 }
