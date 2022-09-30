a0080d66377d08231c792f007cbe942383875adf
Johnams <namsjohnwande@gmail.com>
2022-09-30 19:03:10 +0000

    prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,13 @@
 /**
  * main - prints its name followed by a new line
  * @argc: argc parameter
- * @argv: an array of pointers to the arguments
+ * @argv: an array of a command listed
  *
  * Return: always 0
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	prinf("%s\n", *argv);
+	prinf("%s\n", *argv[0]);
 
 	return (0);
 }
