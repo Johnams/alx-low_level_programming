2893dcf0ecd2fe0cd95b60fa2e8f64a330d44d99
Johnams <namsjohnwande@gmail.com>
2023-01-08 22:24:36 +0300

    task5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -4,10 +4,9 @@
  * hash_table_print - Prints a hash table.
  * @ht: A pointer to the hash table to print.
  *
- * Description: Key/value are printed in the order that they appear in the
- * array of hash table.
+ * Description: Key/value pairs are printed in the order
+ *              they appear in the array of the hash table.
  */
-
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node;
@@ -17,7 +16,7 @@ void hash_table_print(const hash_table_t *ht)
 	if (ht == NULL)
 		return;
 
-	print("{");
+	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
